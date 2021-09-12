440ba86f3f154492bc0c268b77ef7dcb42da75ee
Pete Bachant <petebachant@gmail.com>
2021-09-12 15:08:36 -0400

    Start adding functions to get JHT data

--- jhtdb.py
+++ jhtdb.py
@@ -38,6 +38,7 @@ nz = 2048
 Ti = 0
 Tf = Ti + 1175
 dt = 0.25
+all_times = np.arange(Ti, Tf + dt, dt)
 
 # Create surface
 # nix = round(nx / 4)
@@ -46,6 +47,12 @@ nix = 10
 niy = 10
 niz = 1
 x = np.linspace(x_min, x_max, nix)
+all_x = np.linspace(x_min, x_max, nx)
+all_y = np.linspace(y_min, y_max, ny)
+all_z = np.linspace(z_min, z_max, nz)
+mid_z = 120.0
+mid_x = (x_max - x_min) / 2 + x_min
+mid_y = (y_max - y_min) / 2 + y_min
 # z = np.linspace(z_min, z_max, niz)
 z = np.array([120.0])
 # y = d99i
@@ -58,21 +65,21 @@ points[:, :, 1] = Y.transpose()
 points[:, :, 2] = 120.0
 # points[:, :, 2] = 120.0
 
-# 2D array with single precision values
-points = np.array(points, dtype="float32")
+all_points = np.meshgrid(all_x, all_y, all_z)
 
-points.shape
 
-# Get the velocity gradient at each point
-
-t = Tf
-
-print("Requesting velocity gradients at {0} points...".format(nix * niy))
-result_grad = lTDB.getData(
-    t,
-    points,
-    data_set="transition_bl",
-    sinterp=FD4NoInt,
-    tinterp=temporalInterp,
-    getFunction="getVelocityGradient",
-)
+def get_data_at_points(t, points, quantity="VelocityGradient"):
+    # Convert points to 2-D array with single precision values
+    points = np.array(points, dtype="float32")
+    print(f"Requesting {quantity} at {len(points)} points")
+    if t not in all_times:
+        raise ValueError("Time not in array and interpolation not enabled")
+    res = lTDB.getData(
+        t,
+        points,
+        data_set="transition_bl",
+        sinterp=FD4NoInt,
+        tinterp=temporalInterp,
+        getFunction=f"get{quantity}",
+    )
+    return res
