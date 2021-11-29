e6cfdcb6d9d391ddd1a5b128100a25612a14dac9
Pete Bachant <petebachant@gmail.com>
2021-11-28 19:51:06 -0500

    Copy code for creating stats into module

--- jhtdb.py
+++ jhtdb.py
@@ -6,10 +6,16 @@ import matplotlib
 
 matplotlib.use("nbAgg")
 
-import pyJHTDB
+import os
+
+import h5py
+import matplotlib.pyplot as plt
 import numpy as np
-from tqdm.auto import tqdm
+import pandas as pd
+import pyJHTDB
+from numpy.random import default_rng
 from sqlitedict import SqliteDict
+from tqdm.auto import tqdm
 
 # Get velocity and pressure gradients and Hessians for all points and time average
 
@@ -40,6 +46,9 @@ d99f = 15.0433
 nx = 3320
 ny = 224
 nz = 2048
+
+rho = 1
+nu = 1.25e-3
 
 # Database time duration
 Ti = 0
@@ -72,6 +81,8 @@ points[:, :, 1] = Y.transpose()
 points[:, :, 2] = 120.0
 
 all_points = np.meshgrid(all_x, all_y, all_z)
+
+ALL_STATS_FPATH = "data/jhtdb-transitional-bl/all-stats.h5"
 
 
 def get_data_at_points(
@@ -148,3 +159,149 @@ def get_mean_data_at_points(
             if cache:
                 _cache[key] = ri
     return np.asarray(res)
+
+
+def read_profiles():
+    """Read profile data from JHTDB HDF5 file, and assemble into a dictionary
+    of NumPy arrays.
+    """
+    with h5py.File(
+        "data/jhtdb-transitional-bl/time-ave-profiles.h5", "r"
+    ) as f:
+        data = {}
+        for k in f.keys():
+            kn = k.split("_")[0]
+            if kn.endswith("m"):
+                kn = kn[:-1]
+            data[kn] = f[k][()]
+    # Calculate some finite difference gradients
+    dx = np.gradient(data["x"])
+    dy = np.reshape(np.gradient(data["y"]), (224, 1))
+    # dz = np.gradient(data["z"])
+    # Correct fluctuation terms according to README
+    # >uum is the time-averaged of u*u (not u'*u', where u'=u-um).
+    # >So time-averaged of u'*u'=uum-um*um. Same for other quantities.
+    for dim in ("u", "v", "w"):
+        data[f"{dim}{dim}"] = data[f"{dim}{dim}"] - data[f"{dim}"] ** 2
+    # Calculate gradients
+    data["dpdx"] = np.gradient(data["p"], axis=1) / dx
+    data["duudx"] = np.gradient(data["uu"], axis=1) / dx
+    data["duvdy"] = np.gradient(data["uv"], axis=0) / dy
+    data["dudx"] = np.gradient(data["u"], axis=1) / dx
+    data["dudy"] = np.gradient(data["u"], axis=0) / dy
+    data["d2udx2"] = np.gradient(data["dudx"], axis=1) / dx
+    data["d2udy2"] = np.gradient(data["dudy"], axis=0) / dy
+    data["dpdx"] = np.gradient(data["p"], axis=1) / dx
+    data["dpdy"] = np.gradient(data["p"], axis=0) / dy
+    # data["dwdz"] = np.gradient(data["w"], axis=1) / dz
+    return data
+
+
+def make_points_of_interest(n=200, plot=False, data=None):
+    if data is None:
+        data = read_profiles()
+    rng = default_rng(seed=3423592873429)
+    n = 200
+    yoi = rng.choice(data["y"], size=n)
+    xoi = rng.choice(data["x"][1000:2000], size=n)
+    poi = []
+    for x, y in zip(xoi, yoi):
+        poi.append((x, y, mid_z))
+    poi = np.array(poi)
+    if plot:
+        plt.scatter(poi[:, 0], poi[:, 1])
+    return poi
+
+
+def make_stats(save=True):
+    data = read_profiles()
+    all_points = []
+    for x in data["x"]:
+        for y in data["y"]:
+            all_points.append((x, y, mid_z))
+    poi = make_points_of_interest(data=data)
+    xg, yg = np.meshgrid(data["x"], data["y"])
+    df = pd.DataFrame({"x": xg.flatten(), "y": yg.flatten()})
+    for k, v in data.items():
+        if k in ["x", "y", "z"]:
+            continue
+        if k.startswith("d"):
+            k += "_fd"  # Label as original finite difference
+        df[k] = v.flatten()
+    df = df.set_index(["x", "y"])
+    points = poi[:100]
+    columns = {
+        "VelocityHessian": [  # 18 components
+            # U components
+            "d2udx2",
+            "d2udxdy",
+            "d2udy2",
+            "d2udz2",
+            "d2udxdz",
+            "d2udydz",
+            # V components
+            "d2vdx2",
+            "d2vdxdy",
+            "d2vdxdz",
+            "d2vdy2",
+            "d2vdydz",
+            "d2vdz2",
+            # W components
+            "d2wdx2",
+            "d2wdxdy",
+            "d2wdxdz",
+            "d2wdy2",
+            "d2wdydz",
+            "d2wdz2",
+        ],
+        "PressureHessian": [  # 6 components
+            "d2pdx2",
+            "d2pdxdy",
+            "d2pdxdz",
+            "dp2dy2",
+            "d2pdydz",
+            "d2pdz2",
+        ],
+        "VelocityGradient": [  # 9 components
+            "dudx",
+            "dudy",
+            "dudz",
+            "dvdx",
+            "dvdy",
+            "dvdz",
+            "dwdx",
+            "dwdy",
+            "dwdz",
+        ],
+        "PressureGradient": ["dpdx", "dpdy", "dpdz"],
+    }
+    for quantity, cols in columns.items():
+        res = get_mean_data_at_points(
+            points=points,
+            quantity=quantity,
+            verbose=False,
+            cache_all_times=True,
+        )
+        for p, vals in zip(points, res):
+            vals = vals[0, :]
+            x = p[0]
+            y = p[1]
+            assert x in df.index.get_level_values("x")
+            assert y in df.index.get_level_values("y")
+            for c, v in zip(cols, vals):
+                df.loc[(x, y), c] = v
+    if save:
+        df.to_hdf(ALL_STATS_FPATH, key="data")
+    return df
+
+
+def read_stats():
+    if not os.path.isfile(ALL_STATS_FPATH):
+        import gdown
+
+        url = (
+            "https://drive.google.com/file/d/"
+            "1juON-CqJeVz6jkrs0rr9D0db9i3b6A6a/view?usp=sharing"
+        )
+        gdown.download(url, ALL_STATS_FPATH)
+    return pd.read_hdf(ALL_STATS_FPATH, key="data")
