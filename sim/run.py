8523aa51bee12cf1b13dd2eb71b302115e1204f5
Pete Bachant <petebachant@gmail.com>
2024-10-02 07:32:34 -0700

    Start script for running cases in different dirs

---
+++ sim/run.py
@@ -0,0 +1,86 @@
+"""Script for running the simulation."""
+
+import argparse
+import os
+import subprocess
+import sys
+import shutil
+
+import foampy
+
+WDIR = os.path.dirname(os.path.abspath(__file__))
+BASE_CMD = [
+    "docker",
+    "run",
+    "-it",
+    "--rm",
+    "-w",
+    "/sim",
+    "-v",
+    f"{WDIR}:/sim",
+    "blsim",
+]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--ny",
+        type=int,
+        help="How many cells to create in the y-direction.",
+        default=40,
+    )
+    parser.add_argument(
+        "--turbulence-model",
+        choices=["laminar, k-epsilon", "new"],
+        default="k-epsilon",
+    )
+    parser.add_argument(
+        "--overwrite", "-f", action="store_true", default=False
+    )
+    args = parser.parse_args()
+    case_name = f"{args.turbulence_model}-ny-{args.ny}"
+    case_dir = os.path.join("cases", case_name)
+    # Copy case files into a case directory, deleting anything that might
+    # exist
+    # If the case has already been run, we should see it in the results, or
+    # maybe we should use DVC to sort this out
+    if not args.overwrite and os.path.isdir(case_dir) and os.listdir(case_dir):
+        print("Case directory is not empty; exiting")
+        sys.exit(1)
+    if args.overwrite and os.path.isdir(case_dir):
+        # Delete the case and recreate from scratch
+        shutil.rmtree(case_dir)
+    nx_base = [6, 350, 20, 20, 8]
+    ny_base = 40
+    nx = [int(x * args.ny / ny_base) for x in nx_base]
+    os.makedirs(case_dir, exist_ok=True)
+    system_dir = os.path.join(case_dir, "system")
+    os.makedirs(system_dir, exist_ok=True)
+    blockmeshdict_fpath = os.path.join(system_dir, "blockMeshDict")
+    foampy.fill_template(
+        "system/blockMeshDict.template", blockmeshdict_fpath, nx=nx, ny=args.ny
+    )
+    model_names = {
+        "k-epsilon": "kEpsilon",
+        "laminar": "laminar",
+        "new": "ransFromDns",
+    }
+    constant_dir = os.path.join(case_dir, "constant")
+    os.makedirs(constant_dir, exist_ok=True)
+    foampy.fill_template(
+        "constant/turbulenceProperties.template",
+        os.path.join(constant_dir, "turbulenceProperties"),
+        turbulence_model=model_names[args.turbulence_model],
+    )
+    shutil.copytree("0", os.path.join(case_dir, "0"))
+    # All other non template files to copy over
+    paths = [
+        "constant/transportProperties",
+        "system/controlDict",
+        "system/fvSchemes",
+        "system/fvSolution",
+        "system/sample",
+    ]
+    for path in paths:
+        shutil.copy(path, os.path.join(case_dir, path))
+    # Now post-process the results
