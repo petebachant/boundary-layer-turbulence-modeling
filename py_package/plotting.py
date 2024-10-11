666e8d5412272e09df175e5c6dd370004701bb58
Pete Bachant <petebachant@gmail.com>
2024-10-11 10:36:53 -0700

    Add ability to plot heatmaps that are non-interactive

--- py_package/plotting.py
+++ py_package/plotting.py
@@ -1,8 +1,11 @@
 """Functionality for plotting."""
 
-import plotly.graph_objects as go
-import plotly.express as px
+from __future__ import annotations
+
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from IPython.display import Image
 
 JHTDB_STATS_FPATH = "data/jhtdb-transitional-bl/all-stats.h5"
 
@@ -18,11 +21,12 @@ def plot_heatmap(
     ix_min: int = 0,
     iy_max: int = -1,
     diverging: bool = False,
-) -> go.Figure:
+    interactive: bool = True
+) -> go.Figure | Image:
     """Plot a heatmap of a 2-D array of data matching the shape of the
     JHTDB stats.
     """
-    return px.imshow(
+    fig = px.imshow(
         quantity[:iy_max, ix_min:],
         x=data["x"][ix_min:],
         y=data["y"][:iy_max],
@@ -31,3 +35,6 @@ def plot_heatmap(
         color_continuous_midpoint=0.0 if diverging else None,
         color_continuous_scale="RdBu_r" if diverging else None,
     )
+    if not interactive:
+        return Image(fig.to_image("png"))
+    return fig
